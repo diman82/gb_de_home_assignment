7680378ec7576f45729227aa8ad61fa00d703afe
diman82 <diman82@gmail.com>
2021-09-07 18:24:50 +0300

    almost finished

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
 
-from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql.window import Window
-from pyspark.sql.functions import monotonically_increasing_id, row_number, count, lit, when, col, sum, max, min
+from pyspark.sql.functions import monotonically_increasing_id, row_number, count, col, sum, max, min, explode
 import findspark
 import rootpath
 
@@ -37,29 +35,41 @@ def data_loading():
 
 
 def transformations_and_aggregations():
-    # flatmap the 'struggle_score_types' field, containing array of ditionaries
-    schema = StructType([StructField('struggle_score_rage_click', StringType(), True),
-                         StructField('struggle_score_zoom', StringType(), True),
-                         StructField('struggle_score_http_error', StringType(), True),
-                         StructField('struggle_score_dead_click', StringType(), True),
-                         StructField('struggle_score_app_crash', StringType(), True),
-                         StructField('struggle_score_http_response_time', StringType(), True),
-                         StructField('struggle_score_too_many_tilts', StringType(), True)
-                         ])
+    def flatmap_struggle_score_types(x):
+        temp_dict = dict()
+        for i in range(7):
+            try:
+                temp_dict[i] = x.struggle_score_types[i]
+            except IndexError:
+                temp_dict[i] = {'temp_key': '0.0'}
+        return (temp_dict[0],
+                temp_dict[1],
+                temp_dict[2],
+                temp_dict[3],
+                temp_dict[4],
+                temp_dict[5],
+                temp_dict[6])
 
-    res_df = spark.createDataFrame(data=session_df.select("struggle_score_types").rdd, schema=schema)
-    res_df.printSchema()
+    # flatmap the 'struggle_score_types' field, containing array of ditionaries
+    struggle_score_types_df = session_df.rdd.map(flatmap_struggle_score_types) \
+        .toDF(["struggle_score_rage_click", "struggle_score_zoom",
+               "struggle_score_http_error", "struggle_score_dead_click",
+               "struggle_score_app_crash", "struggle_score_http_response_time", "struggle_score_too_many_tilts"])
+
+    struggle_score_types_df.printSchema()
+    struggle_score_types_df.show()
+
     session_df.drop("struggle_score_types")
 
+    # join sessions_df with struggle_score_types_df
     w = Window.orderBy(monotonically_increasing_id())
     sessions_df = session_df.withColumn("row_index", row_number().over(w))
-    res_df = res_df.withColumn("row_index", row_number().over(w))
+    res_df = struggle_score_types_df.withColumn("row_index", row_number().over(w))
 
     sessions_df = sessions_df.join(res_df, on=["row_index"]).drop("row_index")
     print(sessions_df.columns)
 
-
-    client_flows_df.groupBy("session_uuid")\
+    client_flows_df.groupBy("session_uuid") \
         .agg(count("successful").alias("client_flows_per_session"),
              sum("click_count").alias("click_count_per_session"),
              sum("click_repetition_count").alias("click_count_per_session"),
@@ -68,7 +78,7 @@ def transformations_and_aggregations():
              max("close_reason").alias("most_common_close_reason_per_session"),
              ) \
         .show(truncate=False)
-    client_flows_df.groupBy("session_uuid")\
+    client_flows_df.groupBy("session_uuid") \
         .agg(count("successful").alias("successful_true"),
              sum("click_count").alias("total_click_count_per_session"),
              sum("click_repetition_count").alias("total_click_count_per_session"),
@@ -89,7 +99,6 @@ def transformations_and_aggregations():
         .where(col("successful_false") == False) \
         .show(truncate=False)
 
-
     data_counts = page_loads_df.groupBy("session_uuid") \
         .agg(count("hit_ts").alias("page_loads_per_session"),
              max("struggle_score").alias("most_common_close_reason_per_session"),
@@ -98,14 +107,17 @@ def transformations_and_aggregations():
     page_loads_joined_df = page_loads_df.join(data_counts, "session_uuid").dropDuplicates()
     page_loads_joined_df.show(truncate=False)
 
+    cond1 = [sessions_df.session_uuid == client_flows_df.session_uuid]
+    first_join_df = sessions_df \
+        .join(client_flows_df, cond1, 'inner')
 
+    cond2 = [first_join_df.session_uuid == page_loads_joined_df.session_uuid]
+    final_res_df = sessions_df \
+        .join(page_loads_joined_df, cond2, 'inner')
 
-    cond = [dataset_df.plc_name == metadata_df.plc_name]
-    inner_join_df = dataset_df \
-        .join(metadata_df, cond, 'inner')
+    final_res_df.printSchema()
 
-
-    inner_join_df.coalesce(1).write.mode('overwrite') \
+    final_res_df.coalesce(1).write.mode('overwrite') \
         .option('header', 'true') \
         .parquet("result.parquet")
 
