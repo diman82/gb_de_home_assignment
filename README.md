da212336091c4182659acb9cd2f39d960bd9b1b9
diman82 <diman82@gmail.com>
2021-09-07 12:37:58 +0300

    most agg logic finished

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gb_de_home_assignment
+## Prerequisites
+### Install PySpark on Windows
+https://sparkbyexamples.com/pyspark/how-to-install-and-run-pyspark-on-windows/
+### Install the needed packages in your env
+conda activate <your_python_env>
+pip install -r requirements.txt
--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import os
 
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-import pandas as pd
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql.window import Window
-from pyspark.sql.functions import monotonically_increasing_id, row_number
-from pyspark.sql.functions import lit
-from pyspark.sql.functions import when
+from pyspark.sql.functions import monotonically_increasing_id, row_number, count, lit, when, col, sum, max, min
 import findspark
 import rootpath
 
@@ -28,29 +25,72 @@ def shakespeare_word_count():
     counts.saveAsTextFile("./shakespeareWordCount")
 
 
-def page_loads_per_session():
-    pass
-
-
-def data_loading_and_structuring():
+def data_loading():
     findspark.init()
     findspark.find()
 
+    global spark
     spark = SparkSession \
         .builder \
+        .config("spark.driver.memory", "15g") \
+        .config("spark.driver.maxResultSize ", "0") \
         .appName('gb_de_home_assignment') \
         .getOrCreate()
 
     print("loading parquet files")
-    sessions_df = spark.read.option("badRecordsPath", os.path.join(rootpath.detect(), 'data', 'badRecordsPath')) \
+    global client_flows_df
+    client_flows_df = spark.read.option("badRecordsPath", os.path.join(rootpath.detect(), 'data', 'badRecordsPath')) \
+        .parquet(os.path.join(rootpath.detect(), 'data', 'client_flows.snappy.parquet'), header=True)
+    global session_df
+    session_df = spark.read.option("badRecordsPath", os.path.join(rootpath.detect(), 'data', 'badRecordsPath')) \
         .parquet(os.path.join(rootpath.detect(), 'data', 'sessions.snappy.parquet'), header=True)
-    # sessions_pd_df = pd.read_parquet(os.path.join(rootpath.detect(), 'data', 'sessions.snappy.parquet'))
-    # client_flows_df = spark.read.option("badRecordsPath", os.path.join(rootpath.detect(), 'data', 'badRecordsPath')) \
-    #     .parquet(os.path.join(rootpath.detect(), 'data', 'client_flows.snappy.parquet'), header=False)
-    # page_loads_df = spark.read.option("badRecordsPath", os.path.join(rootpath.detect(), 'data', 'badRecordsPath')) \
-    #     .parquet(os.path.join(rootpath.detect(), 'data', 'page_loads.snappy.parquet'), header=False)
+    global page_loads_df
+    page_loads_df = spark.read.option("badRecordsPath", os.path.join(rootpath.detect(), 'data', 'badRecordsPath')) \
+        .parquet(os.path.join(rootpath.detect(), 'data', 'page_loads.snappy.parquet'), header=True)
+    # global user_action_df
     # user_action_df = spark.read.option("badRecordsPath", os.path.join(rootpath.detect(), 'data', 'badRecordsPath')) \
-    #     .parquet(os.path.join(rootpath.detect(), 'data', 'user_action.snappy.parquet'), header=False)
+    #     .parquet(os.path.join(rootpath.detect(), 'data', 'user_action.snappy.parquet'), header=True)
+
+
+def transformations_and_aggregations():
+    client_flows_df.groupBy("session_uuid")\
+        .agg(count("successful").alias("client_flows_per_session"),
+             sum("click_count").alias("click_count_per_session"),
+             sum("click_repetition_count").alias("click_count_per_session"),
+             sum("field_change_count").alias("field_change_count_per_session"),
+             sum("field_change_repetitions").alias("field_change_repetition_per_session"),
+             max("close_reason").alias("most_common_close_reason_per_session"),
+             ) \
+        .show(truncate=False)
+    client_flows_df.groupBy("session_uuid")\
+        .agg(count("successful").alias("successful_true"),
+             sum("click_count").alias("total_click_count_per_session"),
+             sum("click_repetition_count").alias("total_click_count_per_session"),
+             sum("field_change_count").alias("total_field_change_count_per_session"),
+             sum("field_change_repetitions").alias("total_field_change_repetition_per_session"),
+             max("close_reason").alias("most_common_close_reason_per_session"),
+             ) \
+        .where(col("successful_true") == True) \
+        .show(truncate=False)
+    client_flows_df.groupBy("session_uuid") \
+        .agg(count("successful").alias("successful_false"),
+             sum("click_count").alias("total_click_count_per_session"),
+             sum("click_repetition_count").alias("total_click_count_per_session"),
+             sum("field_change_count").alias("total_field_change_count_per_session"),
+             sum("field_change_repetitions").alias("total_field_change_repetition_per_session"),
+             max("close_reason").alias("most_common_close_reason_per_session"),
+             ) \
+        .where(col("successful_false") == False) \
+        .show(truncate=False)
+
+
+    data_counts = page_loads_df.groupBy("session_uuid") \
+        .agg(count("hit_ts").alias("page_loads_per_session"),
+             max("struggle_score").alias("most_common_close_reason_per_session"),
+             min("struggle_score").alias("most_common_close_reason_per_session"),
+             )
+    page_loads_joined_df = page_loads_df.join(data_counts, "session_uuid").dropDuplicates()
+    page_loads_joined_df.show(truncate=False)
 
     # flatmap the 'struggle_score_types' field, containing array of ditionaries
     schema = StructType([StructField('struggle_score_rage_click', StringType(), True),
@@ -62,36 +102,22 @@ def data_loading_and_structuring():
                          StructField('struggle_score_too_many_tilts', StringType(), True)
                          ])
 
-    struggle_scrore_df = sessions_df.select("struggle_score_types").collect()
-    struggle_scrore_df.printSchema()
-    res_df = spark.createDataFrame(data=struggle_scrore_df.rdd, schema=schema)
+    res_df = spark.createDataFrame(data=session_df.select("struggle_score_types").rdd, schema=schema)
     res_df.printSchema()
-    print(res_df.take(1))
-    sessions_df.drop("struggle_score_types").collect()
+    session_df.drop("struggle_score_types")
 
     w = Window.orderBy(monotonically_increasing_id())
-    sessions_df = sessions_df.withColumn("row_index", row_number().over(w))
+    sessions_df = session_df.withColumn("row_index", row_number().over(w))
     res_df = res_df.withColumn("row_index", row_number().over(w))
 
     sessions_df = sessions_df.join(res_df, on=["row_index"]).drop("row_index")
-    # new_df = sessions_df.union(res_df)
+    print(sessions_df.columns)
 
 
     cond = [dataset_df.plc_name == metadata_df.plc_name]
     inner_join_df = dataset_df \
         .join(metadata_df, cond, 'inner')
 
-    # rename duplicate column names: https://stackoverflow.com/questions/52624888/how-to-write-dataframe-with-duplicate-column-name-into-a-csv-file-in-pyspark
-    newNames = ['name', 'time', 'value', 'site_name', 'plc_name', 'sub_system_name', 'reading_type',
-                'reading_attribute',
-                'machine_type', 'machine_vendor', 'machine_serial_number', 'machine_production_date',
-                'plc_name_duplicate']
-    inner_join_df = inner_join_df.toDF(*newNames)
-
-    print(dataset_df.take(10))
-    print(metadata_df.take(10))
-    print(inner_join_df.take(10))
-    inner_join_df.collect()
 
     inner_join_df.coalesce(1).write.mode('overwrite') \
         .option('header', 'true') \
@@ -103,4 +129,5 @@ def data_loading_and_structuring():
 
 
 if __name__ == "__main__":
-    data_loading_and_structuring()
+    data_loading()
+    transformations_and_aggregations()
